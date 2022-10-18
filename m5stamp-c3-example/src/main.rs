3081cdbb2ff651d7a073845510910ef71996c6f7
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2022-10-18 13:43:40 +0200

    Refactor led colors

--- m5stamp-c3-example/src/main.rs
+++ m5stamp-c3-example/src/main.rs
@@ -10,9 +10,12 @@ pub struct Config {
     wifi_psk: &'static str,
 }
 
-fn main() -> anyhow::Result<()> {
-    use bsc::led::RGB8;
+const GREEN: bsc::led::RGB8 = bsc::led::RGB8::new(0, 50, 0);
+const BLUE: bsc::led::RGB8 = bsc::led::RGB8::new(0, 0, 50);
+const RED: bsc::led::RGB8 = bsc::led::RGB8::new(50, 0, 0);
+const YELLOW: bsc::led::RGB8 = bsc::led::RGB8::new(50, 50, 0);
 
+fn main() -> anyhow::Result<()> {
     let app_config = CONFIG;
 
     esp_idf_sys::link_patches();
@@ -21,21 +24,21 @@ fn main() -> anyhow::Result<()> {
     info!("Starting");
 
     let mut led = bsc::led::WS2812RMT::new()?;
-    led.set_pixel(RGB8::new(50, 50, 0))?;
+    led.set_pixel(YELLOW)?;
 
     let _wifi = match bsc::wifi::wifi(app_config.wifi_ssid, app_config.wifi_psk) {
         Ok(inner) => inner,
         Err(err) => {
-            led.set_pixel(RGB8::new(50, 0, 0))?;
+            led.set_pixel(RED)?;
             anyhow::bail!("could not connect to Wi-Fi network: {:?}", err)
         }
     };
 
     loop {
-        led.set_pixel(RGB8::new(0, 0, 50))?;
+        led.set_pixel(BLUE)?;
         std::thread::sleep(std::time::Duration::from_secs(1));
         info!("Loop");
-        led.set_pixel(RGB8::new(0, 50, 0))?;
+        led.set_pixel(GREEN)?;
         std::thread::sleep(std::time::Duration::from_secs(1));
     }
 }
